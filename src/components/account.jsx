48f2eae984154e2d1cf759f9cd8a376784446efb
Robert Allred <rallred84@gmail.com>
2023-07-09 11:14:38 -0500

    Fixed some merging errors

--- src/components/account.jsx
+++ src/components/account.jsx
@@ -1,25 +1,42 @@
+import { useOutletContext } from "react-router-dom";
+import { Button } from "@mui/material";
+
 const Account = () => {
+  const { user, setToken, setUser } = useOutletContext();
+
+  function admin() {
+    window.location.href = "/admin";
+  }
+
   function handleLogout() {
     localStorage.removeItem("token");
     setToken("");
     setUser({});
   }
+
   return (
     <div>
       <h1>Account Details</h1>
       <div>
         <p>
           Email:
-          {/* {user.email} */}
+          {user.email}
         </p>
         <p>
-          Username:
-          {/* {user.username} */}
+          Name:
+          {user.firstName}
         </p>
       </div>
       <div>
         <h1>Order History</h1>
       </div>
+      {user.isAdmin && (
+        <>
+          <div id="profile-header">
+            <Button onClick={admin}>Admin Dashboard</Button>
+          </div>
+        </>
+      )}
       <div>
         <button onClick={handleLogout}>Logout</button>
       </div>
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,94 +4,129 @@ import { Link, useNavigate, useOutletContext } from "react-router-dom";
 import { createUser, loginUser } from "../api";
 
 export default function Login() {
-    const [loginEmail, setLoginEmail] = useState("");
-    const [loginPassword, setLoginPassword] = useState("");
-    const [loginConfirm, setLoginConfirm] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [loginConfirm, setLoginConfirm] = useState("");
 
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [registerEmail, setRegisterEmail] = useState("");
-    const [registerPassword, setRegisterPassword] = useState("");
-    const [registerConfirm, setRegisterConfirm] = useState("");
-    const [error, setError] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerConfirm, setRegisterConfirm] = useState("");
+  const [error, setError] = useState("");
 
-    const { setToken } = useOutletContext();
+  const { setToken } = useOutletContext();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    async function handleLogin(event) {
-        event.preventDefault();
-        console.log(loginEmail, loginPassword, loginConfirm);
+  async function handleLogin(event) {
+    event.preventDefault();
+    console.log(loginEmail, loginPassword, loginConfirm);
 
-        if (registerPassword !== registerConfirm) {
-         setError("Passwords don't match!");
-         return;
-        }
+    if (registerPassword !== registerConfirm) {
+      setError("Passwords don't match!");
+      return;
+    }
 
-        const login = await loginUser(loginEmail, loginPassword);
-        console.log(login);
+    const login = await loginUser(loginEmail, loginPassword);
+    console.log(login);
 
-        if (!login.success) {
-         setError(login.error.message);
-         return;
-        }
+    if (!login.success) {
+      setError(login.error.message);
+      return;
+    }
 
-        setToken(login.data.token);
-        localStorage.setItem("token", login.data.token);
-        navigate("/");
-      }
+    setToken(login.data.token);
+    localStorage.setItem("token", login.data.token);
+    navigate("/account");
+  }
 
-    async function handleRegister(event) {
-        event.preventDefault();
-      //   console.log(firstName, lastName, registerEmail, registerPassword, registerConfirm);
+  async function handleRegister(event) {
+    event.preventDefault();
+    //   console.log(firstName, lastName, registerEmail, registerPassword, registerConfirm);
 
-        if (registerPassword !== registerConfirm) {
-         setError("Passwords don't match!");
-         return;
-        }
+    if (registerPassword !== registerConfirm) {
+      setError("Passwords don't match!");
+      return;
+    }
 
-        const result = await createUser(registerEmail, registerPassword, firstName, lastName)
-      //   console.log(result)
+    const result = await createUser(
+      registerEmail,
+      registerPassword,
+      firstName,
+      lastName
+    );
+    //   console.log(result)
 
-        if (!result.success) {
-         setError(result.error.message);
-         return;
-        }
+    if (!result.success) {
+      setError(result.error.message);
+      return;
+    }
 
-        setToken(result.data.token);
-        localStorage.setItem("token", result.data.token);
-        navigate("/");
-      }
+    setToken(result.data.token);
+    localStorage.setItem("token", result.data.token);
+    navigate("/account");
+  }
 
-    return(
-        <div>
-         <h1 id="headline">Log-in</h1>
-         <form onSubmit={handleLogin} id="logging-in">
-            <input placeholder="email" onChange={(event) => setLoginEmail(event.target.value)} value={loginEmail}
-               />
-            <input placeholder="password" type="password" onChange={(event) => setLoginPassword(event.target.value)} value={loginPassword}
-               />
-            <input placeholder="confirm" type="password" onChange={(event) => setLoginConfirm(event.target.value)} value={loginConfirm}
-               />
-            <div id="login-err">{error}</div>
-            <button id="log-in">Login</button>
-         </form>
-         <div>No Account?<Link to={`/register`}>Register Here</Link></div>
-         <h1 id="headline">Register</h1>
-         <form onSubmit={handleRegister} id="registering">
-            <input placeholder="first name" onChange={(event) => setFirstName(event.target.value)} value={firstName}
-               />
-            <input placeholder="last name" onChange={(event) => setLastName(event.target.value)} value={lastName}
-               />
-            <input placeholder="email" onChange={(event) => setRegisterEmail(event.target.value)} value={registerEmail}
-               />
-            <input placeholder="password" type="password" onChange={(event) => setRegisterPassword(event.target.value)} value={registerPassword}
-               />
-            <input placeholder="confirm" type="password" onChange={(event) => setRegisterConfirm(event.target.value)} value={registerConfirm}
-               />
-            <div id="reg-err">{error}</div>
-            <button id="register">Register</button>
-         </form>
-       </div>
-       )  
+  return (
+    <div>
+      <h1 id="headline">Log-in</h1>
+      <form onSubmit={handleLogin} id="logging-in">
+        <input
+          placeholder="email"
+          onChange={(event) => setLoginEmail(event.target.value)}
+          value={loginEmail}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          onChange={(event) => setLoginPassword(event.target.value)}
+          value={loginPassword}
+        />
+        <input
+          placeholder="confirm"
+          type="password"
+          onChange={(event) => setLoginConfirm(event.target.value)}
+          value={loginConfirm}
+        />
+        <div id="login-err">{error}</div>
+        <button id="log-in">Login</button>
+      </form>
+      <div>
+        No Account?<Link to={`/register`}>Register Here</Link>
+      </div>
+      <h1 id="headline">Register</h1>
+      <form onSubmit={handleRegister} id="registering">
+        <input
+          placeholder="first name"
+          onChange={(event) => setFirstName(event.target.value)}
+          value={firstName}
+        />
+        <input
+          placeholder="last name"
+          onChange={(event) => setLastName(event.target.value)}
+          value={lastName}
+        />
+        <input
+          placeholder="email"
+          onChange={(event) => setRegisterEmail(event.target.value)}
+          value={registerEmail}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          onChange={(event) => setRegisterPassword(event.target.value)}
+          value={registerPassword}
+        />
+        <input
+          placeholder="confirm"
+          type="password"
+          onChange={(event) => setRegisterConfirm(event.target.value)}
+          value={registerConfirm}
+        />
+        <div id="reg-err">{error}</div>
+        <button id="register">Register</button>
+      </form>
+    </div>
+  );
 }
