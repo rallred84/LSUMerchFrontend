19dd6fe52be31cfccea09045046e0310f4e71b50
Robert Allred <rallred84@gmail.com>
2023-07-09 01:38:48 -0500

    Committing Michael's changes to products.jsx
    Also started writing logic for uploading photos

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import { createProduct } from "../api";
+import ImageUpload from "./imageUpload";
 
 const Admin = () => {
   const [name, setName] = useState("");
@@ -17,7 +18,14 @@ const Admin = () => {
     event.preventDefault();
     try {
       if (user.isAdmin) {
-        const result = await createProduct(token, name, description, price, stockQuantity, size);
+        const result = await createProduct(
+          token,
+          name,
+          description,
+          price,
+          stockQuantity,
+          size
+        );
         console.log(result);
         setProducts([result, ...products]);
         setName("");
@@ -25,26 +33,47 @@ const Admin = () => {
         setPrice(0);
         setStockQuantity(0);
         setSize("");
-        navigate("/")
+        navigate("/");
       }
     } catch (err) {
       console.error(err);
     }
   }
 
-
   return (
     <div>
       <h1>Admin</h1>
       <h2>Create Products</h2>
       <form onSubmit={handleSubmit} id="new-product">
-         <input placeholder="Name" onChange={(event) => setName(event.target.value)} value={name} />
-         <input placeholder="Description" onChange={(event) => setDescription(event.target.value)} value={description} />
-         <input placeholder="Price" onChange={(event) => setPrice(event.target.value)} value={price} />
-         <input placeholder="Stock Quantity" onChange={(event) => setStockQuantity(event.target.value)} value={stockQuantity} />
-         <input placeholder="Size" onChange={(event) => setSize(event.target.value)} value={size} />
-         <button id="create-product">Create Product</button>
+        <input
+          placeholder="Name"
+          onChange={(event) => setName(event.target.value)}
+          value={name}
+        />
+        <input
+          placeholder="Description"
+          onChange={(event) => setDescription(event.target.value)}
+          value={description}
+        />
+        <input
+          placeholder="Price"
+          onChange={(event) => setPrice(event.target.value)}
+          value={price}
+        />
+        <input
+          placeholder="Stock Quantity"
+          onChange={(event) => setStockQuantity(event.target.value)}
+          value={stockQuantity}
+        />
+        <input
+          placeholder="Size"
+          onChange={(event) => setSize(event.target.value)}
+          value={size}
+        />
+        <button id="create-product">Create Product</button>
       </form>
+
+      <ImageUpload />
     </div>
   );
 };
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,9 +1,62 @@
-const Products = () => {
+import { useOutletContext, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BASE_URL, getAllProducts } from "../api";
+import { useParams } from "react-router-dom";
+
+export default function Products() {
+  const { productId } = useParams();
+  const { products } = useOutletContext();
+
+  const [loading, setLoading] = useState(true);
+  const [productName, setProductName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  const singleProduct = async () => {
+    try {
+      setLoading(true);
+      const response = await getAllProducts(productId);
+
+      let productName = response[productId - 1].name;
+      let price = response[productId - 1].price;
+      let description = response[productId - 1].description;
+
+      setProductName(productName);
+      setPrice(price);
+      setDescription(description);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    singleProduct();
+  }, [productId]);
+
+  function addToCart() {
+    alert("Product added to cart");
+  }
+  if (loading) {
+    return <> Loading...</>;
+  }
   return (
-    <div>
-      <h1>Products</h1>
+    <div id="product-pg">
+      <h1 className="product-name">{productName}</h1>
+      <div className="product-description">{description}</div>
+      <div className="single-product-image">
+        <img src="https://placekitten.com/640/360" alt={products.name} />
+      </div>
+      <div className="single-product-price">{price}</div>
+      <div className="product-page-btn">
+        {" "}
+        <button className="single-product-btn" onClick={addToCart}>
+          Add to Cart
+        </button>
+        <Link to="/">
+          <button className="back-btn">Back to Products</button>
+        </Link>
+      </div>
     </div>
   );
-};
-
-export default Products;
+}
