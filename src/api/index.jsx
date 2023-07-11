9da7956944790fcbcc052261f126d5d2fc789814
Robert Allred <rallred84@gmail.com>
2023-07-10 22:40:13 -0500

    restored sydneys api calls

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -169,16 +169,6 @@ export async function deleteProduct(token, productId) {
   }
 }
 
-<<<<<<< HEAD
-/////////////////
-
-// POST /orders
-
-export async function createNewCart(token) {
-  try {
-    const response = await fetch(`${BASE_URL}/orders`, {
-      method: "POST",
-=======
 //////////////////
 
 /*orders*/
@@ -187,49 +177,19 @@ export async function createNewCart(token) {
 export async function getOrders(token) {
   try {
     const response = await fetch(`${BASE_URL}/orders`, {
->>>>>>> 5c3c3c027f408320f12962856d888f9a7c90b798
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     });
     const result = await response.json();
-<<<<<<< HEAD
-    return result;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-// POST /orders_products/add
-
-export async function addProductToCart(token, productId) {
-  try {
-    const response = await fetch(`${BASE_URL}/orders_products/add`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        productId,
-        quantity: 1,
-      }),
-    });
-    const result = await response.json();
-    console.log(result);
-    const info = result.data.cart;
-=======
     const info = result.data.orders;
     // console.log(result);
->>>>>>> 5c3c3c027f408320f12962856d888f9a7c90b798
     return info;
   } catch (err) {
     console.error(err);
   }
 }
-<<<<<<< HEAD
-=======
 
 export async function getUsersOrders(token, userId) {
   try {
@@ -247,4 +207,3 @@ export async function getUsersOrders(token, userId) {
     console.error(err);
   }
 }
->>>>>>> 5c3c3c027f408320f12962856d888f9a7c90b798
