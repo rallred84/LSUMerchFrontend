b095c2799d82ff2dc06b939b4fc5d6b2a7eed49f
MichaelWilson03 <michaelbwilson2012@gmail.com>
2023-07-12 16:29:41 -0500

    styling cards and product page

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,14 +1,36 @@
 import { useOutletContext, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { BASE_URL, getAllProducts } from "../api";
+import { getAllProducts } from "../api";
 import { useParams } from "react-router-dom";
 import { Button } from "@mui/material";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
-import { ThemeProvider } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#3c1053",
+          color: "white",
+          textTransform: "none",
+          borderRadius: "4px",
+          padding: "8px 16px",
+          fontWeight: "bold",
+          transition: "background-color 0.3s ease-in-out",
+
+          "&:hover": {
+            backgroundColor: "#d29f13",
+          },
+        },
+      },
+    },
+  },
+});
 
 export default function Products() {
   const { productId } = useParams();
-  const { theme } = useOutletContext();
 
   const [loading, setLoading] = useState(true);
 
@@ -43,21 +65,29 @@ export default function Products() {
   return (
     <div id="product-pg">
       <h1 className="single-product-name">{product.name}</h1>
-      <div className="product-description">{product.description}</div>
-      <div className="single-product-image">
-        <img src={product.imageURL} alt={product.name} />
-      </div>
-      <div className="single-product-price">Price: {product.price}</div>
-      <div className="product-page-btn">
-        <ThemeProvider theme={theme}>
-          <Button onClick={addToCart} className="single-product-btn">
-            Add to Cart
-          </Button>{" "}
-          <br />
-          <Link to="/">
-          <Button className="single-product-btn">Back to Products</Button>{" "}
-          </Link>
-        </ThemeProvider>
+      <div className="product-details">
+        <div className="single-product-image">
+          <img src={product.imageURL} alt={product.name} />
+        </div>
+        <div className="product-info">
+          <div className="single-product-price">Price: {product.price}</div>
+          <div className="product-description">{product.description}</div>
+          <div className="product-page-btn">
+            <ThemeProvider theme={theme}>
+              <ButtonGroup orientation="vertical">
+                <Button onClick={addToCart} className="single-product-btn">
+                  Add to Cart
+                </Button>{" "}
+                <br />
+                <Link to="/">
+                  <Button className="single-product-btn">
+                    Back to Products
+                  </Button>{" "}
+                </Link>
+              </ButtonGroup>
+            </ThemeProvider>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,8 +1,10 @@
 import { Link, useOutletContext } from "react-router-dom";
 import { addProductToCart, createNewCart, getProfile } from "../api";
+import { ThemeProvider } from "@mui/material/styles";
+import { Button } from "@mui/material";
 
 export default function Welcome() {
-  const { products, user, setUser, token } = useOutletContext();
+  const { products, user, setUser, token, theme } = useOutletContext();
 
   if (!products) {
     return <></>;
@@ -17,40 +19,37 @@ export default function Welcome() {
     const fetchMe = await getProfile(token);
     setUser(fetchMe);
   }
+
   return (
     <div id="home_page">
       <h1 id="welcome">Welcome To The Tigers Den!</h1>
-      <div className="cards-container" key={products}>
-        {products.length > 0
-          ? products.map((product) => {
-              // console.log(product);
-              return (
-                <div id="card" key={product.id}>
-                  <div className="products-pg">
-                    <Link
-                      to={`/products/${product.id}`}
-                      style={{ textDecoration: "none" }}
-                    >
-                      <h1 className="product-name">{product.name}</h1>
-
-                      <div className="product-image">
-                        <img src={product.imageURL} alt={products.name} />
-                      </div>
-                      <div className="product-price">
-                        Price: {product.price}
-                      </div>
-                    </Link>
-                    <button
-                      className="product-btn"
-                      onClick={() => addToCart(product.id)}
-                    >
-                      Add to Cart{" "}
-                    </button>
-                  </div>
+      <div className="cards-container">
+        {products.length > 0 ? (
+          products.map((product) => (
+            <div id="card" key={product.id} className="product-card">
+              <Link
+                to={`/products/${product.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <h1 className="product-name">{product.name}</h1>
+                <div className="product-image">
+                  <img src={product.imageURL} alt={products.name} />
                 </div>
-              );
-            })
-          : null}
+                <div className="product-price">Price: {product.price}</div>
+              </Link>
+              <ThemeProvider theme={theme}>
+                <Button
+                  onClick={() => addToCart(product.id)}
+                  className="product-btn"
+                >
+                  Add to Cart
+                </Button>{" "}
+              </ThemeProvider>
+            </div>
+          ))
+        ) : (
+          <div>No products available</div>
+        )}
       </div>
     </div>
   );
