b4cbf781ce8457f858e64da7a3b76afd78f91063
MichaelWilson03 <michaelbwilson2012@gmail.com>
2023-07-13 19:59:43 -0500

    account page changes

--- src/components/account.jsx
+++ src/components/account.jsx
@@ -1,9 +1,11 @@
 import { Link, useOutletContext, useParams } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
 import { deleteReview } from "../api";
+import "../css/account.css";
+import { WidthFull } from "@mui/icons-material";
 
 const Account = () => {
-  const { user, setToken, setUser, token } = useOutletContext();
+  const { user, setToken, setUser, token, theme } = useOutletContext();
 
   function admin() {
     window.location.href = "/admin";
@@ -21,72 +23,96 @@ const Account = () => {
   }
 
   return (
-    <div>
+    <div className="account-page">
       {user.isAdmin && (
-        <>
-          <div id="profile-header">
-            <Button onClick={admin}>Admin Dashboard</Button>
-          </div>
-        </>
+        <div className="admin-dashboard">
+          <Button onClick={admin} variant="contained" theme={theme}>
+            Admin Dashboard
+          </Button>
+        </div>
       )}
-      <h1>Account Details</h1>
-      <div>
+      <h1 className="account-heading">Account Details</h1>
+      <div className="account-details">
         <p>
-          Email:
-          {user.email}
+          <span>Email:</span> {user.email}
         </p>
         <p>
-          Name:
-          {user.firstName}
+          <span>Name:</span> {user.firstName}
         </p>
       </div>
-      <div>
+      <div className="order-history">
         <h1>Order History</h1>
-        {user.orders && user.orders.length
-         ? user.orders.map((order) => {
-          // console.log(order);
-          return (
-            <div id="order-id" key={order.id}>
-              <div className="order-id">Id: {order.id}</div>
-              <div className="order-status">Status: {order.orderStatus}</div>
+        {user.orders && user.orders.length ? (
+          user.orders.map((order) => (
+            <div className="order" key={order.id}>
+              <div className="order-id">
+                <span>Id:</span> {order.id}
+              </div>
+              <div className="order-status">
+                <span>Status:</span> {order.orderStatus}
+              </div>
               {order.products && order.products.length
-              ? order.products.map((product) => {
-                return (
-                  <div key={product.id}>
-                    <div className="product">{product.name}</div>
-                    <div className="product-quantity">Quantity: {product.quantity}</div>
-                  </div>
-                )
-              })
-            : null }
-              <div className="order-price">Total: {order.totalPrice}</div>
+                ? order.products.map((product) => (
+                    <div className="product" key={product.id}>
+                      <div>{product.name}</div>
+                      <div className="product-quantity">
+                        <span>Quantity:</span> {product.quantity}
+                      </div>
+                    </div>
+                  ))
+                : null}
+              <div className="order-price">
+                <span>Total:</span> {order.totalPrice}
+              </div>
             </div>
-          );
-         })
-        : null}
+          ))
+        ) : (
+          <div className="no-orders">No order history found.</div>
+        )}
       </div>
-      <div>
+      <div className="my-reviews">
         <h1>My Reviews</h1>
-        {user.reviews && user.reviews.length
-         ? user.reviews.map((review) => {
-          // console.log(review);
-          return (
-            <div id="review-id" key={review.id}>
+        {user.reviews && user.reviews.length ? (
+          user.reviews.map((review) => (
+            <div className="review" key={review.id}>
               <div className="review-product">{review.productName}</div>
               <div className="review-message">{review.message}</div>
-              <div className="review-rating">Rating: {review.rating}</div>
-              <div className="review-date">Date: {review.date}</div>
-              <Link to={`/${review.productId}/editreview`}>
-              <button>Edit Review</button>
-              </Link>
-              <button onClick={() => handleDelete(review.productId)}>Delete Review</button>
+              <div className="review-rating">
+                <span>Rating:</span> {review.rating}
+              </div>
+              <div className="review-date">
+                <span>Date:</span> {review.date}
+              </div>
+              <ButtonGroup className="review-buttons" orientation="vertical">
+                <Link to={`/${review.productId}/editreview`}>
+                  <Button
+                    variant="outlined"
+                    theme={theme}
+                    sx={{ width: "100%" }}
+                  >
+                    Edit Review
+                  </Button>
+                </Link>
+                <br />
+                <Button
+                  onClick={() => handleDelete(review.productId)}
+                  variant="outlined"
+                  theme={theme}
+                  sx={{ width: "100%" }}
+                >
+                  Delete Review
+                </Button>
+              </ButtonGroup>
             </div>
-          );
-         })
-        : null}
+          ))
+        ) : (
+          <div className="no-reviews">No reviews found.</div>
+        )}
       </div>
-      <div>
-        <button onClick={handleLogout}>Logout</button>
+      <div className="logout-button">
+        <Button onClick={handleLogout} variant="outlined" theme={theme}>
+          Logout
+        </Button>
       </div>
     </div>
   );
