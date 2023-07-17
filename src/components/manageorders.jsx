53e033333c5dce630262e48e3ad38e228e0ebaec
sweakleyw <sweakleyworks@gmail.com>
2023-07-17 19:56:30 -0400

    manage orders

--- src/components/manageorders.jsx
+++ src/components/manageorders.jsx
@@ -1,20 +1,52 @@
-import { useOutletContext } from "react-router"
+import { useEffect } from "react";
+import { useOutletContext, useParams } from "react-router";
+import { getOrders, orderComplete } from "../api";
+import { Button } from "@mui/material";
+
+import '../css/ManageOrders.css'
 
 export default function ManageOrders() {
-    const {token, orders, setOrders} = useOutletContext();
+    const {token, orders, setOrders, user, theme} = useOutletContext();
+    // console.log(orders)
+
+    useEffect(() => {
+      if (user.isAdmin) {
+        const fetchOrders = async () => {
+          const fetchAllOrders = await getOrders(token);
+          console.log(fetchAllOrders);
+          setOrders(fetchAllOrders);
+        };
+        fetchOrders();
+      }
+    }, [user]);
+
+    async function handleOrder(orderId) {
+      const result = await orderComplete(token, orderId);
+      console.log(result)
+    }
 
     return (
-        <div>
+        <div className="manage-orders-page">
         <h1>Manage Orders</h1>
           {orders && orders.length
           ? orders.map((order) => {
              return (
-               <div id="admin-orders" key={order.id}>
+               <div className="admin-orders" key={order.id}>
                 <div className="order-id">Id: {order.id}</div>
                 <div className="order-user">User Id: {order.userId}</div>
                 <div className="order-status">Status: {order.orderStatus}</div>
                 <div className="order-price">Total: {order.totalPrice}</div>  
-                <button>Update</button>
+                {order.orderStatus === "Order Placed" && (
+                <> 
+                  <Button
+                    onClick={() => {handleOrder(order.id)}}
+                    variant="outlined"
+                    theme={theme}
+                  >
+                  Complete
+                  </Button>
+                </> 
+                )}
                </div>
               )
             })
--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -90,6 +90,35 @@ export default function Welcome() {
           <div>No products available</div>
         )}
       </div>
+      <div>
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+        <img
+         alt="Creative Commons License"
+         style={{ borderWidth: 0 }}
+         src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png"
+        />
+      </a>
+      <br />
+      <span
+        xmlns:dct="http://purl.org/dc/terms/"
+        href="http://purl.org/dc/dcmitype/StillImage"
+        property="dct:title"
+        rel="dct:type"
+      >
+        Tigers Den
+      </span>{" "}
+        is licensed under a{" "}
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+        Creative Commons Attribution-ShareAlike 4.0 International License
+      </a>
+      <br />
+        Based on a work at{" "}
+      <a href="https://commons.wikimedia.org/wiki/File:LSU_football_game.jpg">
+        Getawaypaul27
+      </a>
+      <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>,
+      via Wikimedia Commons
+      </div>
     </div>
   );
 }
