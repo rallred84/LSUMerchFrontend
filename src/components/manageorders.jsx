9862318b049ca47354506f111d2820cb8f5be943
Robert Allred <rallred84@gmail.com>
2023-07-17 19:16:11 -0500

    Made minor changes to manage orders page
    Top of page filters orders that have been placed, then a list of completed orders. Did not include any user carts

--- src/components/manageorders.jsx
+++ src/components/manageorders.jsx
@@ -1,56 +1,88 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useOutletContext, useParams } from "react-router";
 import { getOrders, orderComplete } from "../api";
 import { Button } from "@mui/material";
 
-import '../css/ManageOrders.css'
+import "../css/ManageOrders.css";
 
 export default function ManageOrders() {
-    const {token, orders, setOrders, user, theme} = useOutletContext();
-    // console.log(orders)
+  const { token, orders, setOrders, user, theme } = useOutletContext();
+  // console.log(orders)
 
-    useEffect(() => {
-      if (user.isAdmin) {
-        const fetchOrders = async () => {
-          const fetchAllOrders = await getOrders(token);
-          console.log(fetchAllOrders);
-          setOrders(fetchAllOrders);
-        };
-        fetchOrders();
-      }
-    }, [user]);
+  const [placedOrders, setPlacedOrders] = useState([]);
+  const [completedOrders, setCompletedOrders] = useState([]);
 
-    async function handleOrder(orderId) {
-      const result = await orderComplete(token, orderId);
-      console.log(result)
+  useEffect(() => {
+    if (user.isAdmin) {
+      const fetchOrders = async () => {
+        const fetchAllOrders = await getOrders(token);
+        console.log(fetchAllOrders);
+        setOrders(fetchAllOrders);
+      };
+      fetchOrders();
     }
+  }, [user]);
 
-    return (
-        <div className="manage-orders-page">
-        <h1>Manage Orders</h1>
-          {orders && orders.length
-          ? orders.map((order) => {
-             return (
-               <div className="admin-orders" key={order.id}>
+  useEffect(() => {
+    console.log(orders);
+    const placedOrders = orders.filter(
+      (order) => order.orderStatus === "Order Placed"
+    );
+    setPlacedOrders(placedOrders);
+
+    const completedOrders = orders.filter(
+      (order) => order.orderStatus === "Order Complete"
+    );
+    setCompletedOrders(completedOrders);
+  }, [orders]);
+
+  async function handleOrder(orderId) {
+    const result = await orderComplete(token, orderId);
+    console.log(result);
+  }
+
+  return (
+    <div className="manage-orders-page">
+      <h1>Orders To Ship</h1>
+      {placedOrders && placedOrders.length
+        ? placedOrders.map((order) => {
+            return (
+              <div className="admin-orders" key={order.id}>
                 <div className="order-id">Id: {order.id}</div>
                 <div className="order-user">User Id: {order.userId}</div>
                 <div className="order-status">Status: {order.orderStatus}</div>
-                <div className="order-price">Total: {order.totalPrice}</div>  
+                <div className="order-price">Total: {order.totalPrice}</div>
                 {order.orderStatus === "Order Placed" && (
-                <> 
-                  <Button
-                    onClick={() => {handleOrder(order.id)}}
-                    variant="outlined"
-                    theme={theme}
-                  >
-                  Complete
-                  </Button>
-                </> 
+                  <>
+                    <Button
+                      onClick={() => {
+                        handleOrder(order.id);
+                      }}
+                      variant="outlined"
+                      theme={theme}
+                    >
+                      Complete
+                    </Button>
+                  </>
                 )}
-               </div>
-              )
-            })
-         : null}
-        </div>
-    )
+              </div>
+            );
+          })
+        : null}
+
+      <h1>Completed Orders</h1>
+      {completedOrders && orders.length
+        ? completedOrders.map((order) => {
+            return (
+              <div className="admin-orders" key={order.id}>
+                <div className="order-id">Id: {order.id}</div>
+                <div className="order-user">User Id: {order.userId}</div>
+                <div className="order-status">Status: {order.orderStatus}</div>
+                <div className="order-price">Total: {order.totalPrice}</div>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
 }
